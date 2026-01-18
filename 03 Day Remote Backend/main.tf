68dc223d4a419b25f98cf42cafc52168440a1c6d
Manoranjan-Sethi <manoranjansethi97@gmail.com>
2026-01-19 00:05:26 +0530

    setting remote backend with s3

---
+++ 03 Day Remote Backend/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+    backend "s3" {
+    bucket = "myuniquebucket-terraformano"
+    key    = "dev/terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create s3 bucket
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "my-tf-unique-bucket2111-06-27"
+
+    tags = {
+        Name        = "My bucket 2.0"
+        Environment = "Dev"
+    }
+}
